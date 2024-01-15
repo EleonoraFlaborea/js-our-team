c85b377fc737a39356d919cd5d71f83b9ebb0ab3
EleonoraFlaborea <flaboreaeleonora@gmail.com>
2024-01-15 18:50:32 +0100

    add object array

--- js/script.js
+++ js/script.js
@@ -11,4 +11,64 @@ BONUS 1:
 Trasformare la stringa foto in una immagine effettiva
 BONUS 2:
 Organizzare i singoli membri in card/schede prendendo a esempio uno dei due layout proposti
+
+DATI
+Wayne Barnett |	Founder & CEO        |	wayne-barnett-founder-ceo.jpg
+Angela Caroll |	Chief Editor         |	angela-caroll-chief-editor.jpg
+Walter Gordon |	Office Manager       |	walter-gordon-office-manager.jpg
+Angela Lopez  |	Social Media Manager |	angela-lopez-social-media-manager.jpg
+Scott Estrada |	Developer            |	scott-estrada-developer.jpg
+Barbara Ramos |	Graphic Designer     |	barbara-ramos-graphic-designer.jpg
 */
+
+const team = document.getElementById('profile')
+
+//Creo l’array di oggetti
+const profile = [
+    {
+        name: 'Wayne Barnett',
+        role: 'Chief Editor',
+        picture: 'wayne-barnett-founder-ceo.jpg',
+    },
+
+    {
+        name: 'Angela Caroll',
+        role: 'Founder & CEO',
+        picture: 'angela-caroll-chief-editor.jpg',
+    },
+
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        picture: 'walter-gordon-office-manager.jpg',
+    },
+
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        picture: 'angela-lopez-social-media-manager.jpg',
+    },
+
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        picture: 'scott-estrada-developer.jpg',
+    },
+
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        picture: 'barbara-ramos-graphic-designer.jpg',
+    }
+]
+
+//TODO:
+/*
+MILESTONE 1:
+Stampare su console le informazioni di nome, ruolo e la stringa della foto
+MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe
+BONUS 1:
+Trasformare la stringa foto in una immagine effettiva
+BONUS 2:
+Organizzare i singoli membri in card/schede prendendo a esempio uno dei due layout proposti*/
